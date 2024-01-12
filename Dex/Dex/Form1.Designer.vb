4275bd0425f64561ef75a439849123f9c974775e
Nolan Cain <nbcai604@learner.dvusd.org>
2024-01-12 12:42:07 -0700

    Read File

---
+++ Dex/Dex/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RavensToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BillsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChiefsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TexansToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BrownsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DolphinsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SteelersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ErsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CowboysToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuccaneersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EaglesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RamsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PackersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Field1 = New System.Windows.Forms.TextBox()
+        Me.Field2 = New System.Windows.Forms.TextBox()
+        Me.Field3 = New System.Windows.Forms.TextBox()
+        Me.Field4 = New System.Windows.Forms.TextBox()
+        Me.Field5 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.SaveToolStripMenuItem, Me.RavensToolStripMenuItem, Me.BillsToolStripMenuItem, Me.ChiefsToolStripMenuItem, Me.TexansToolStripMenuItem, Me.BrownsToolStripMenuItem, Me.DolphinsToolStripMenuItem, Me.SteelersToolStripMenuItem, Me.ErsToolStripMenuItem, Me.CowboysToolStripMenuItem, Me.LionsToolStripMenuItem, Me.BuccaneersToolStripMenuItem, Me.EaglesToolStripMenuItem, Me.RamsToolStripMenuItem, Me.PackersToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'RavensToolStripMenuItem
+        '
+        Me.RavensToolStripMenuItem.Name = "RavensToolStripMenuItem"
+        Me.RavensToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.RavensToolStripMenuItem.Text = "Ravens"
+        '
+        'BillsToolStripMenuItem
+        '
+        Me.BillsToolStripMenuItem.Name = "BillsToolStripMenuItem"
+        Me.BillsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.BillsToolStripMenuItem.Text = "Bills"
+        '
+        'ChiefsToolStripMenuItem
+        '
+        Me.ChiefsToolStripMenuItem.Name = "ChiefsToolStripMenuItem"
+        Me.ChiefsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.ChiefsToolStripMenuItem.Text = "Chiefs"
+        '
+        'TexansToolStripMenuItem
+        '
+        Me.TexansToolStripMenuItem.Name = "TexansToolStripMenuItem"
+        Me.TexansToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.TexansToolStripMenuItem.Text = "Texans"
+        '
+        'BrownsToolStripMenuItem
+        '
+        Me.BrownsToolStripMenuItem.Name = "BrownsToolStripMenuItem"
+        Me.BrownsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.BrownsToolStripMenuItem.Text = "Browns"
+        '
+        'DolphinsToolStripMenuItem
+        '
+        Me.DolphinsToolStripMenuItem.Name = "DolphinsToolStripMenuItem"
+        Me.DolphinsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.DolphinsToolStripMenuItem.Text = "Dolphins"
+        '
+        'SteelersToolStripMenuItem
+        '
+        Me.SteelersToolStripMenuItem.Name = "SteelersToolStripMenuItem"
+        Me.SteelersToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.SteelersToolStripMenuItem.Text = "Steelers"
+        '
+        'ErsToolStripMenuItem
+        '
+        Me.ErsToolStripMenuItem.Name = "ErsToolStripMenuItem"
+        Me.ErsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.ErsToolStripMenuItem.Text = "49ers"
+        '
+        'CowboysToolStripMenuItem
+        '
+        Me.CowboysToolStripMenuItem.Name = "CowboysToolStripMenuItem"
+        Me.CowboysToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.CowboysToolStripMenuItem.Text = "Cowboys"
+        '
+        'LionsToolStripMenuItem
+        '
+        Me.LionsToolStripMenuItem.Name = "LionsToolStripMenuItem"
+        Me.LionsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.LionsToolStripMenuItem.Text = "Lions"
+        '
+        'BuccaneersToolStripMenuItem
+        '
+        Me.BuccaneersToolStripMenuItem.Name = "BuccaneersToolStripMenuItem"
+        Me.BuccaneersToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.BuccaneersToolStripMenuItem.Text = "Buccaneers"
+        '
+        'EaglesToolStripMenuItem
+        '
+        Me.EaglesToolStripMenuItem.Name = "EaglesToolStripMenuItem"
+        Me.EaglesToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.EaglesToolStripMenuItem.Text = "Eagles"
+        '
+        'RamsToolStripMenuItem
+        '
+        Me.RamsToolStripMenuItem.Name = "RamsToolStripMenuItem"
+        Me.RamsToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.RamsToolStripMenuItem.Text = "Rams"
+        '
+        'PackersToolStripMenuItem
+        '
+        Me.PackersToolStripMenuItem.Name = "PackersToolStripMenuItem"
+        Me.PackersToolStripMenuItem.Size = New System.Drawing.Size(166, 26)
+        Me.PackersToolStripMenuItem.Text = "Packers"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(110, 297)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Pro Bowlers"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(110, 251)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "WC Matchup"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(110, 197)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Reg. Wins"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(110, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Seed"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(110, 98)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Confrence"
+        '
+        'Field1
+        '
+        Me.Field1.Location = New System.Drawing.Point(192, 98)
+        Me.Field1.Name = "Field1"
+        Me.Field1.Size = New System.Drawing.Size(125, 27)
+        Me.Field1.TabIndex = 10
+        '
+        'Field2
+        '
+        Me.Field2.Location = New System.Drawing.Point(158, 145)
+        Me.Field2.Name = "Field2"
+        Me.Field2.Size = New System.Drawing.Size(125, 27)
+        Me.Field2.TabIndex = 11
+        '
+        'Field3
+        '
+        Me.Field3.Location = New System.Drawing.Point(190, 194)
+        Me.Field3.Name = "Field3"
+        Me.Field3.Size = New System.Drawing.Size(125, 27)
+        Me.Field3.TabIndex = 12
+        '
+        'Field4
+        '
+        Me.Field4.Location = New System.Drawing.Point(210, 244)
+        Me.Field4.Name = "Field4"
+        Me.Field4.Size = New System.Drawing.Size(125, 27)
+        Me.Field4.TabIndex = 13
+        '
+        'Field5
+        '
+        Me.Field5.Location = New System.Drawing.Point(203, 297)
+        Me.Field5.Name = "Field5"
+        Me.Field5.Size = New System.Drawing.Size(125, 27)
+        Me.Field5.TabIndex = 14
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(383, 84)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(295, 240)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 15
+        Me.PictureBox1.TabStop = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Field5)
+        Me.Controls.Add(Me.Field4)
+        Me.Controls.Add(Me.Field3)
+        Me.Controls.Add(Me.Field2)
+        Me.Controls.Add(Me.Field1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RavensToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BillsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ChiefsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TexansToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BrownsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DolphinsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SteelersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ErsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CowboysToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BuccaneersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EaglesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RamsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PackersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Field1 As TextBox
+    Friend WithEvents Field2 As TextBox
+    Friend WithEvents Field3 As TextBox
+    Friend WithEvents Field4 As TextBox
+    Friend WithEvents Field5 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
